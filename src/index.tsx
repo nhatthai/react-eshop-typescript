e4612232eff24dd782cddcb21eed9254f8b1dcee
Nhat Thai <thaiquangnhatit@gmail.com>
2022-09-24 14:44:35 +0700

    Add package

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <Auth0Provider
+    domain="dev-i5eh5nau.us.auth0.com"
+    clientId="VXYHq8KLHdVszLaTAtuljRJIaCI79BrO"
+    redirectUri={window.location.origin}
+  >
     <App />
-  </React.StrictMode>
+  </Auth0Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
