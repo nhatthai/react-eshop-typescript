78419f5e72745d01da188adcb535037eceab2f84
Nhat Thai <nhat.thaiquang@asnet.com.vn>
2022-09-26 09:41:27 +0700

    Update config

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,14 @@
 /// <reference types="react-scripts" />
+declare global {
+    namespace NodeJS {
+      interface ProcessEnv {
+        REACT_APP_AUTH0_DOMAIN: string;
+        REACT_APP_AUTH0_CLIENTID: string
+        NODE_ENV: 'development' | 'production';
+      }
+    }
+}
+
+// If this file has no import/export statements (i.e. is a script)
+// convert it into a module by adding an empty export statement.
+export {}
